390a444142ec1d8696cc01c5026588e40e8b3c64
chenzhi <chenzhi@qq.com>
2021-12-10 18:25:10 +0900

    Response　call	的理解

--- app/src/main/java/com/example/databindingtest/RetrofitService.kt
+++ app/src/main/java/com/example/databindingtest/RetrofitService.kt
@@ -1,15 +1,28 @@
 package com.example.databindingtest
 
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 import retrofit2.http.GET
+
+
+
 
 interface RetrofitService {
 
     //从"https://howtodoandroid.com/"中，获取的movielist.json数据，并封装成List<Movie>格式
     @GET("movielist.json")
     suspend fun getAllMovies(): Response<List<Movie>>
+
+    //Response	只是发送，没有后期处理的。
+    //call	最后有callback返回的。
+    //但是如果我们用的是RxJava或Coroutines，就可以不用管了，因为他们都带有返回
+    @GET("movielist.json")
+    fun getDog(): Call<List<Movie>>
 
     companion object {
         //本类RetrofitService的一个静态对象，暂时是空的
